f2f5d57161c13cc12d24ea980b4cf4342d77023c
arulson <jebbyarulson@gmail.com>
2025-11-13 01:14:10 -0800

    new version with tracking enabled

---
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/arulson/keymap.c
@@ -0,0 +1,169 @@
+/**
+ * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+ * Copyright 2023 casuanoob <casuanoob@hotmail.com> (@casuanoob)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum dilemma_keymap_layers {
+    LAYER_BASE = 0,
+    LAYER_FUNCTION,
+    LAYER_NAVIGATION,
+    LAYER_MEDIA,
+    LAYER_POINTER,
+    LAYER_NUMERAL,
+    LAYER_SYMBOLS,
+};
+
+// Automatically enable sniping-mode on the pointer layer.
+// #define DILEMMA_AUTO_SNIPING_ON_LAYER LAYER_POINTER
+
+#define ESC_MED LT(LAYER_MEDIA, KC_ESC)
+#define SPC_NAV LT(LAYER_NAVIGATION, KC_SPC)
+#define F_FUN LT(LAYER_FUNCTION, KC_F)
+#define ENT_SYM LT(LAYER_SYMBOLS, KC_ENT)
+#define BSP_NUM LT(LAYER_NUMERAL, KC_BSPC)
+#define PT_P LT(LAYER_POINTER, KC_P)
+#define PT_B LT(LAYER_POINTER, KC_B)
+
+#ifndef POINTING_DEVICE_ENABLE
+#    define DRGSCRL KC_NO
+#    define DPI_MOD KC_NO
+#    define S_D_MOD KC_NO
+#    define SNIPING KC_NO
+#endif // !POINTING_DEVICE_ENABLE
+
+// clang-format off
+/** \brief QWERTY layout (3 rows, 10 columns). */
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [LAYER_BASE] = LAYOUT_split_3x5_3(
+       KC_Q,          KC_X,         KC_G,         KC_M,         KC_K,     KC_SCLN,  KC_COMM,      KC_QUOT,      KC_J,         KC_Z,
+       LGUI_T(KC_N),  LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_W,     KC_DOT,   LCTL_T(KC_A), LSFT_T(KC_E), LALT_T(KC_I), LGUI_T(KC_H),
+       PT_P,          KC_L,         KC_C,         KC_D,         KC_V,     KC_TAB,   KC_U,         KC_O,         KC_Y,         PT_B,
+                                    F_FUN,        BSP_NUM,      ESC_MED,  QK_CAPS_WORD_TOGGLE,  SPC_NAV,      ENT_SYM
+  ),
+/*
+ * Layers used on the Dilemma.
+ *
+ * These layers started off heavily inspired by the Miryoku layout, but trimmed
+ * down and tailored for a stock experience that is meant to be fundation for
+ * further personalization.
+ *
+ * See https://github.com/manna-harbour/miryoku for the original layout.
+ */
+
+/**
+ * \brief Function layer.
+ *
+ * Secondary right-hand layer has function keys mirroring the numerals on the
+ * primary layer with extras on the pinkie column, plus system keys on the inner
+ * column. App is on the tertiary thumb key and other thumb keys are duplicated
+ * from the base layer to enable auto-repeat.
+ */
+  [LAYER_FUNCTION] = LAYOUT_split_3x5_3(
+    _______, _______, _______, _______, _______,   KC_MUTE,   KC_F7, KC_F8, KC_F9,  KC_F12,
+    _______, _______, _______, _______, _______,   KC_VOLU,   KC_F4, KC_F5, KC_F6,  KC_F11,
+    _______, _______, _______, _______, _______,   KC_VOLD,   KC_F1, KC_F2, KC_F3,  KC_F10,
+                      _______, _______, _______,   KC_MPRV,   KC_MNXT, _______
+  ),
+
+/**
+ * \brief Navigation layer.
+ *
+ * Primary right-hand layer (left home thumb) is navigation and editing. Cursor
+ * keys are on the home position, line and page movement below, clipboard above,
+ * caps lock and insert on the inner column. Thumb keys are duplicated from the
+ * base layer to avoid having to layer change mid edit and to enable auto-repeat.
+ */
+  [LAYER_NAVIGATION] = LAYOUT_split_3x5_3(
+    _______, _______, _______, _______, KC_INS,     _______, KC_BTN1, KC_BTN2, KC_BTN3, _______,
+    KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_BRIU,    _______, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI,
+    KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_BRID,    _______, _______, _______, _______, _______,
+                      _______, KC_ESC,  KC_DEL,     _______, _______, _______
+  ),
+/**
+ * \brief Media layer.
+ *
+ * Tertiary left- and right-hand layer is media and RGB control.  This layer is
+ * symmetrical to accomodate the left- and right-hand trackball.
+ */
+  [LAYER_MEDIA] = LAYOUT_split_3x5_3(
+    XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX,
+    KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                      _______, KC_MPLY, KC_MSTP, KC_MSTP, KC_MPLY, KC_MUTE
+  ),
+
+/** \brief Mouse emulation and pointer functions. */
+  [LAYER_POINTER] = LAYOUT_split_3x5_3(
+    QK_BOOT,  EE_CLR, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX,  EE_CLR, QK_BOOT,
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+    _______, DRGSCRL, SNIPING, KC_BTN3, XXXXXXX, XXXXXXX, KC_BTN3, SNIPING, DRGSCRL, _______,
+                      KC_BTN3, KC_BTN2, KC_BTN1, KC_BTN1, KC_BTN2, KC_BTN3
+  ),
+
+/**
+ * \brief Numeral layout.
+ *
+ * Primary left-hand layer (right home thumb) is numerals and symbols. Numerals
+ * are in the standard numpad locations with symbols in the remaining positions.
+ * `KC_DOT` is duplicated from the base layer.
+ */
+  [LAYER_NUMERAL] = LAYOUT_split_3x5_3(
+    _______,   _______,   _______,   _______,   _______,   KC_PLUS, KC_7,   KC_8,   KC_9,   KC_ASTR,
+    KC_LGUI,   KC_LALT,   KC_LSFT,   KC_LCTL,   _______,   KC_DOT,  KC_4,   KC_5,   KC_6,   KC_0,
+    _______,   _______,   _______,   _______,   _______,   KC_EQL,  KC_1,   KC_2,   KC_3,   KC_SLSH,
+                          _______,   _______,   _______,   KC_MPLY, KC_SPC, _______
+  ),
+
+/**
+ * \brief Symbols layer.
+ *
+ * Secondary left-hand layer has shifted symbols in the same locations to reduce
+ * chording when using mods with shifted symbols. `KC_LPRN` is duplicated next to
+ * `KC_RPRN`.
+ */
+  [LAYER_SYMBOLS] = LAYOUT_split_3x5_3(
+    _______,   _______,   _______,  _______,  _______,   _______, _______, _______, _______, _______, 
+    KC_LBRC,   KC_RBRC,   KC_LCBR,  KC_RCBR,  KC_TILD,   _______, _______, _______, _______, _______, 
+    KC_LABK,   KC_RABK,   KC_LPRN,  KC_RPRN,  KC_GRV,    _______, _______, _______, _______, _______, 
+                          _______,  KC_UNDS,  KC_BSLS,   _______, _______, _______
+  ),
+};
+// clang-format on
+
+#ifdef POINTING_DEVICE_ENABLE
+#    ifdef DILEMMA_AUTO_SNIPING_ON_LAYER
+layer_state_t layer_state_set_user(layer_state_t state) {
+    dilemma_set_pointer_sniping_enabled(layer_state_cmp(state, DILEMMA_AUTO_SNIPING_ON_LAYER));
+    return state;
+}
+#    endif // DILEMMA_AUTO_SNIPING_ON_LAYER
+#endif     // POINTING_DEVICE_ENABLE
+
+#ifdef ENCODER_MAP_ENABLE
+// clang-format off
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [LAYER_BASE]       = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [LAYER_FUNCTION]   = {ENCODER_CCW_CW(KC_DOWN, KC_UP),    ENCODER_CCW_CW(KC_LEFT, KC_RGHT)},
+    [LAYER_NAVIGATION] = {ENCODER_CCW_CW(KC_PGDN, KC_PGUP),  ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
+    [LAYER_MEDIA]      = {ENCODER_CCW_CW(KC_PGDN, KC_PGUP),  ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
+    [LAYER_POINTER]    = {ENCODER_CCW_CW(RM_HUED, RM_HUEU),  ENCODER_CCW_CW(RM_SATD, RM_SATU)},
+    [LAYER_NUMERAL]    = {ENCODER_CCW_CW(RM_VALD, RM_VALU),  ENCODER_CCW_CW(RM_SPDD, RM_SPDU)},
+    [LAYER_SYMBOLS]    = {ENCODER_CCW_CW(RM_PREV, RM_NEXT),  ENCODER_CCW_CW(KC_LEFT, KC_RGHT)},
+};
+// clang-format on
+#endif // ENCODER_MAP_ENABLE
